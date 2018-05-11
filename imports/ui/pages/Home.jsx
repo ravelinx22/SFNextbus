bc4c927e0bf4380174d25cc05488df6e93b8e5de
William Ravelo <drummerwilliam@gmail.com>
2018-05-10 21:32:10 -0500

    added  basic structure

---
+++ imports/ui/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import * as d3 from "d3";
+// import {  } from "../../data.js";
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	componentDidMount() {
+	}
+
+	render() {
+		return(
+			<h1>Hello World</h1>
+		);
+	}
+}
